e6ddfd04edc44f38b09eb7fc440bb38519008e84
Lawrence Berry <lawrence.berry@gmail.com>
2021-03-27 16:03:12 +0000

    add in default sleep time

--- lib/media_magnet/parser/reddit/downloadable_image.rb
+++ lib/media_magnet/parser/reddit/downloadable_image.rb
@@ -3,14 +3,16 @@ module MediaMagnet
     class Reddit
       class DownloadableImage < MediaMagnet::Parser::Reddit::Image
         include MediaMagnet::Base::Downloadable
+        
+        DEFAULT_SLEEP_TIME = 0.1
 
         def initialize(data:, opts: {})
           super
           @url = url
           @dir = opts[:dir]
           @local_name = filename
-          @no_sleep = true unless opts[:sleep_time]
-          @sleep_time = opts[:sleep_time]
+          @no_sleep = true and return if opts[:no_sleep]
+          @sleep_time = opts[:sleep_time] || DEFAULT_SLEEP_TIME
         end
       end 
     end 
